7bd8e09bffa8176141350e547a811b8ff783ab9b
Prasadkpd <2019cs078@stu.ucsc.lk>
2022-08-26 08:50:00 +0530

    initial commit

---
+++ spring_boot_react_emp/src/test/java/com/prasadkpd/spring_boot_react_emp/SpringBootReactEmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasadkpd.spring_boot_react_emp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactEmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
